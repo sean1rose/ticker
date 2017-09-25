9c197cd2f8e5d4f82028b647057ffc7fb3bc9f23
Sean Rose <sean1rose@gmail.com>
2017-09-25 02:07:41 -0700

    basic ticker and breaking news update UI implemented

--- src/components/App.js
+++ src/components/App.js
@@ -13,18 +13,59 @@ const T = new Twit({
 
 let stream = T.stream('user', { with: ['coupleogoats'] });
 
-const INITIAL_STATE = {
+let oneDayAgo = new Date().getTime() - 86400000;
+let twelveHoursAgo = new Date().getTime() - 43200000;
+let tweetsInLastHalfDay = [];
+let INITIAL_STATE = {
   tweetArr: [],
-  tweetObj: {}
-};
+  tweetObj: {},
+  lastTweet: {}
+}
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = INITIAL_STATE;
   }
-
+  
   componentDidMount() {
+    console.log('T -> ', T);
+
+    // BACK LOG
+    T.get('statuses/user_timeline', { screen_name: 'FantasyLabsNFL', count: 20, exclude_replies: true, include_rts: false, trim_user: true}, (err, data, response) => {
+      data.forEach((item) => {
+        // console.log('0 item - ', item.text);
+        let currentDate = new Date(item.created_at).getTime();
+        // console.log('current date - ', currentDate);
+        if (currentDate > twelveHoursAgo){
+          // run check to make sure tweet meets news-worthy criteria
+          
+          if (!item.text.includes("@")){
+            // if tweet text includes url, cut out that url
+            let strArr = item.text.split(' ');
+            let numberOfWords = strArr.length;
+            let lastWord = strArr[numberOfWords - 1];
+            let rejoinedStr;
+            if (lastWord.includes("http")){
+              strArr.pop();
+              // console.log('---> strArr after pop- ', strArr);
+              rejoinedStr = strArr.join(' ');
+              // console.log('*** - ', rejoinedStr);
+              item.text = rejoinedStr;
+            }
+
+            tweetsInLastHalfDay.push(item);
+          }
+        }
+      });
+      console.log('tweets in last 1/2 day - ', tweetsInLastHalfDay);
+      this.setState({
+        tweetArr: tweetsInLastHalfDay
+      });
+      console.log('this.state.tweetArr [FINAL LIST OF INITIAL BACK LOG!!!!]- ', this.state.tweetArr);
+    });
+
+    // NEW TWEET STREAM
     console.log('mounted, stream - ', stream, this.state);
     stream.on('tweet', (tweet) => {
       console.log('1 finaltwtt b4 - ', this.state.tweetArr, this.state.tweetObj);
@@ -34,26 +75,31 @@ class App extends React.Component {
         tweetObj: {
           ...this.state.tweetObj,
           [tweet.id_str]: tweet
-        }
+        },
+        lastTweet: tweet
       });
       console.log('3 state - ', this.state.tweetArr, this.state.tweetObj);
     });  
   }
 
   render() {
-    const newsItems = [{id_str: 1, text: 'Dez Bryant will play tomorrow'}, {id_str: 2, text: 'Larry Fitzgerald is listed as questionable'}].map((item) => {
-    // const newsItems = this.state.tweetArr.map((item) => {
+    // const newsItems = [{id_str: 1, text: 'Dez Bryant will play tomorrow'}, {id_str: 2, text: 'Larry Fitzgerald is listed as questionable'}].map((item) => {
+    const newsItems = this.state.tweetArr.map((item) => {
       return (
         <div className="ticker__item" key={item.id_str}>{item.text}</div>
       );
     });
+
+    let breakingNewsItem = null;
+    if (this.state.lastTweet.text){
+      breakingNewsItem = <div><h1 className="centered">BREAKING!</h1><h3 className="centered">{this.state.lastTweet.text}</h3></div>
+    } else {
+      breakingNewsItem = <div></div>
+    }
+
     return (
       <div>
-        <h1>Hello, Electron!</h1>
-        <p>I hope you enjoy using basic-electron-react-boilerplate to start your dev off right!</p>
-        <ul>
-          {newsItems}
-        </ul>
+        {breakingNewsItem}
         <div className="ticker-wrap">
           <div className="ticker">
             {newsItems}
